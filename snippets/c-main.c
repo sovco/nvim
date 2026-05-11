3a85c1ebdee404cd44a45c4bb8543f122877f562
sovco <no@email.stop>
2026-05-11 21:04:15 +0200

    scripts added

---
+++ snippets/c-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[]) {
+    return EXIT_SUCCESS;
+}
